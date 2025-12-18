68cf044923f3cc5c93f4707a90b987103e9c5276
unknown <tareqhasan382@gmail.com>
2025-12-19 05:56:57 +0600

    Integrate Redux Toolkit query

---
+++ src/src/redux/auth/authSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Parse localStorage once
+const authString = localStorage.getItem("murmur");
+const auth = authString ? JSON.parse(authString) : null;
+// const authString = localStorage.getItem("auth") ?? null;
+// const auth = authString ? JSON.parse(authString) : null;
+const initialState = {
+  accessToken: auth ? auth.accessToken : null,
+  user: auth ? auth.user : undefined,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "murmur",
+  initialState,
+  reducers: {
+    userLoggedIn: (state, action) => {
+      //   console.log("action:", action.payload); //  accessToken
+
+      state.accessToken = action.payload.accessToken;
+      state.user = action.payload.user;
+      localStorage.setItem(
+        "murmur",
+        JSON.stringify({
+          accessToken: action.payload.accessToken,
+          user: action.payload.user,
+        })
+      );
+    },
+    userLoggedOut: (state) => {
+      // Remove item from localStorage
+      localStorage.removeItem("murmur");
+      // Reset state values
+      state.accessToken = null;
+      state.user = undefined;
+    },
+  },
+});
+
+export const { userLoggedIn, userLoggedOut } = authSlice.actions;
+export default authSlice.reducer;
